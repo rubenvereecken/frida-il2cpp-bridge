3dd85858ba0fce2939f768190f2a099d034fcabb
Ruben Vereecken <ruben@20tech.io>
2025-03-08 11:26:44 +0000

    Updated TS target, prettier config, yarn

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    printWidth: 120,
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+}
+
+export default config
