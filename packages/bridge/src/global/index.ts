3f1c1df0f33ff4ea0684e66c206fbed28a10b4f6
Ruben Vereecken <ruben@20tech.io>
2025-08-16 10:18:10 +0100

    Added an Il2Cpp global for easy frida debugging

---
+++ packages/bridge/src/global/index.ts
@@ -0,0 +1,36 @@
+// Import everything from the main ESM exports
+import * as application from '../application.js';
+import * as corlib from '../corlib.js';
+import * as dump from '../dump.js';
+import * as exceptionListener from '../exception-listener.js';
+import * as filters from '../filters.js';
+import * as gc from '../gc.js';
+import * as memory from '../memory.js';
+import * as module from '../module.js';
+import * as perform from '../perform.js';
+import * as tracer from '../tracer.js';
+
+// Import barrel exports
+import * as structs from '../structs/index.js';
+import * as native from '../native/index.js';
+
+// Export everything under the Il2Cpp namespace
+const Il2Cpp = {
+    ...structs,
+    ...native,
+    ...application,
+    ...corlib,
+    ...dump,
+    ...exceptionListener,
+    ...filters,
+    ...gc,
+    ...memory,
+    ...module,
+    ...perform,
+    ...tracer,
+};
+
+(globalThis as any).Il2Cpp = Il2Cpp;
+
+// Default export for easy importing
+export default Il2Cpp;
